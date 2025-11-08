571dbb62a9525ede2422d0eb5b6413944a14ccfe
Andddrexx <andrexquintero03@gmail.com>
2025-11-08 16:08:21 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -361,7 +361,11 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
             <div className="relative">
-     
+               <img
+                src={logogold}
+                alt="Logo de Mayik Solution"
+                className="relative rounded-2xl shadow-2xl w-full h-100 object-contain "
+              />
             </div>
 
             <div>
